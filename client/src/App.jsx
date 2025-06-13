2a8809478424c2cdb0923180c88d94890e9c937c
Rahul Umrao <rahul@Rahuls-MacBook-Pro.local>
2025-06-14 03:39:14 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>Hello World!!</>
+
+    // <Router>
+    //   <Routes>
+    //     <Route path="/home" element={<ArchitectureDoc />} />
+    //   </Routes>
+    // </Router>
+  );
+}
+
+export default App;
